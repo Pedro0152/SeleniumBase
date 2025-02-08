4300fc9839fb249f1cc41184bf339a36db86353d
Pedro0152 <126337213+Pedro0152@users.noreply.github.com>
2025-02-08 01:11:07 -0300

    Update idle_mmo.py

--- examples/my_examples/idle_mmo.py
+++ examples/my_examples/idle_mmo.py
@@ -5,19 +5,44 @@ import os
 
 TOKEN = os.environ["TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
-USERNAME = os.environ["USERNAME"]
+EMAIL = os.environ["EMAIL"]
 PASSWORD = os.environ["PASSWORD"]
 
-URL_OVERVIEW ='https://2moons.cu/game.php?page=overview'
+URL_LOGIN ='https://web.idle-mmo.com/login'
+URL_OVERVIEW = 'https://web.idle-mmo.com/@Kresh'
 URL_HASH = 'https://2moons.cu/game.php?page=overview'
 LOGIN_BUTTON = '/html/body/section/div[2]/div/div[2]/div/div[2]/form/div[4]/button'
 
-user = 'input[id="email"]'
+# Login
+EMAIL_INPUT = 'input[id="email"]'
+PASSWORD_INPUT = 'input[id="password"]'
+SUBMIT_BUTTON = 'button[id="submit"]'
+DAILY_REWARD = 'button[class="text-yellow-400"]'
+
+def login(sb):
+    sb.activate_cdp_mode(URL_LOGIN)
+    try:
+        sb.cdp.press_keys(EMAIL_INPUT, EMAIL)
+        sb.cdp.press_keys(PASSWORD_INPUT, PASSWORD)
+        sb.cdp.click(SUBMIT_BUTTON)
+        print('login!')
+        sb.cdp.sleep(2)
+    except Exception as e:
+        print('Login fail', e)
+
+def get_daily_reward(sb):
+    try:
+        sb.cdp.click(DAILY_REWARD)
+        print('Daily Reward')
+        sb.cdp.sleep(2)
+    except Exception as e:
+        print('Daily Reward fail', e)
 
 def main(SB):
     with SB(uc=True,test=True) as sb:
         login(sb)
-        update_officier(sb)
+        get_daily_reward(sb)
+        sb.cdp.open(URL_OVERVIEW)
         message = 'Github Action Done!'
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
         print(requests.get(url).json())
